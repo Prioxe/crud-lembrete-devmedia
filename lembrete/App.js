19579b0c449491294f2a4fc85df752265f188121
Goku <jose_netopio_12@hotmail.com>
2019-09-08 04:33:00 -0300

    7. Preenchendo a lista

--- lembrete/App.js
+++ lembrete/App.js
@@ -1,48 +1,48 @@
-  import React, { Component } from 'react';
-  import {
-    StyleSheet,
-    View,
-    Text,
-    Button
-  } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button
+} from 'react-native';
 
 
 
-  export default class App extends Component {
+export default class App extends Component {
 
-    state = {
-      lembretes: []
-    }
+  state = {
+    lembretes: []
+  }
 
-    componentDidMount(){
-      fetch('https://deviup.com.br:3001/api/lembrete', {method: 'GET'})
-        .then(T => T.json())
-        .then(lembretes => this.setState({ lembretes }))
-    }
+  componentDidMount() {
+    fetch('https://deviup.com.br:3001/api/lembrete', { method: 'GET' })
+      .then(T => T.json())
+      .then(lembretes => this.setState({ lembretes }))
+  }
 
 
-    render() {
-      return (
-        <View >
-          <Text>Lembre</Text>
+  render() {
+
+    const { lembretes } = this.state
+
+
+    return (
+      <View >
+          <Text>Lembrete</Text>
           <Button title='Adicionar' onPress={() => console.log('Criou o lembrete')} />
           <View>
-            <View>
-              <Text>Lembrete 1</Text>
-              <Button title='Editar' onPress={() => console.log('Editou')} />
-              <Button title='Excluir' onPress={() => console.log('Excluiu')} />
-            </View>
-            <View>
-              
-              <Text>Lembrete 2</Text>
-              <Button title='Editar' onPress={() => console.log('Editou')} />
-              <Button title='Excluir' onPress={() => console.log('Excluiu')} />
-            </View>
+            {lembretes.map((lembrete, key) => (
+              <View key={key}>
+                <Text>{lembrete.conteudo}</Text>
+                <Button title='Editar' onPress={() => console.log('editou')} />
+                <Button title='Excluir' onPress={() => console.log('excluiu')} />
+              </View>  
+            ))}
           </View>
-        </View>
-      )
-    }
+      </View>
+    )
   }
+}
 
 // const styles = StyleSheet.create({
 //   container:{
